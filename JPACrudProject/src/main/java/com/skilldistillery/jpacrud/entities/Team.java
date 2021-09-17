3f3b07cfa7b34056b80258b9a3bcfc3881bfe584
cgardner90 <calebgardner2015@gmail.com>
2021-09-17 17:50:04 -0600

    Configured Project, Teamjava, junit

---
+++ JPACrudProject/src/main/java/com/skilldistillery/jpacrud/entities/Team.java
@@ -0,0 +1,36 @@
+package com.skilldistillery.jpacrud.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	// update to make database fields
+	
+	public Team() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
